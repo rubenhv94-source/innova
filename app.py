bcbc063fd4113ced56110564b074523c90d1f405
rubenhv94-source <rubenhv.94@gmail.com>
2025-10-06 13:11:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -372,13 +372,17 @@ if pagina_actual == "Resumen":
     col3.metric("👨‍👧‍👧 Equipo VA", f"{equipo_va:,}".replace(",", "."))
     col4.metric("⚠️ Por asignar", f"{por_asignar:,}".replace(",", "."))
 
-    fig_estado = grafico_estado_con_meta(df_filtrado, "Resumen", 0)
-    st.plotly_chart(fig_estado, use_container_width=True)
+    colR_fig1, colR_fig2 = st.columns(2)
 
-    avance = df_filtrado["estado_carpeta"].str.lower().isin(["auditada", "aprobada", "calificada"]).sum()
-    total = len(df_filtrado)
-    fig_gauge = grafico_avance_total(total, avance)
-    st.plotly_chart(fig_gauge, use_container_width=True)
+    with colR_fig1:
+        avance = df_filtrado["estado_carpeta"].str.lower().isin(["auditada", "aprobada", "calificada"]).sum()
+        total = len(df_filtrado)
+        fig_gauge = grafico_avance_total(total, avance)
+        st.plotly_chart(fig_gauge, use_container_width=True)
+    
+    with colR_fig2:
+        fig_estado = grafico_estado_con_meta(df_filtrado, "Resumen", 0)
+        st.plotly_chart(fig_estado, use_container_width=True)
 
 # ============ MÓDULOS CON METAS Y ATRASOS ============
 def modulo_vista(nombre_modulo: str):
