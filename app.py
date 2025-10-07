04a2821499231fbb94e402ebed8a2020ceadb704
rubenhv94-source <rubenhv.94@gmail.com>
2025-10-07 15:31:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -612,7 +612,7 @@ def modulo_vista(nombre_modulo: str):
     
         elif nombre_modulo == 'Supervisores':           
             if analista_label_2:
-                cxa1, cxa2, cxa3 = cx2.columns(2)
+                cxa1, cxa2, cxa3 = cx2.columns(3)
                 with cxa1:
                     custom_metric("💯 Equipo", equipo_label) 
                 with cxa2:
@@ -628,16 +628,18 @@ def modulo_vista(nombre_modulo: str):
                 custom_metric("👩‍💼 Profesional", auditor_label)
       
         else:
-            with cx1:
-                custom_metric("💯 Equipo", equipo_label)            
             if analista_label_2:
-                cxa1, cxa2 = cx2.columns(2)
+                cxa1, cxa2, cxa3 = cx2.columns(3)
                 with cxa1:
-                    custom_metric("👨‍💻 Analista2", analista_label_2)
+                    custom_metric("💯 Equipo", equipo_label) 
                 with cxa2:
+                    custom_metric("👨‍💻 Analista2", analista_label_2)
+                with cxa3:
                     custom_metric("👨‍💻 Analista2", analista_label_2)                    
             else:
                 with cx1:
+                    custom_metric("💯 Equipo", equipo_label)                
+                with cx2:
                     custom_metric("👨‍💻 Analista", analista_label_1)                    
             with cx3:
                 custom_metric("🕵️‍♀️ Supervisor", supervisor_label)
