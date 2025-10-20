cc4b05bb2e28592c0ae1f3adff9b03546d9edcc4
rubenhv94-source <rubenhv.94@gmail.com>
2025-10-20 01:35:32 -0500

    Update dashborad.py

--- INPEC/dashborad.py
+++ INPEC/dashborad.py
@@ -38,7 +38,7 @@ st.markdown("""
 </style>
 """, unsafe_allow_html=True)
 
-COLOR_PALETTE = px.colors.sequential.Green[::-1]
+COLOR_PALETTE = px.colors.sequential.Greens[::-1]
 
 # ===================================
 # 📥 CARGA DE DATOS
