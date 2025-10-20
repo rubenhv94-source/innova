12ddab8bb59286d67aed0630d905a4f426e4740b
rubenhv94-source <rubenhv.94@gmail.com>
2025-10-20 02:52:15 -0500

    Update dashborad.py

--- INPEC/dashborad.py
+++ INPEC/dashborad.py
@@ -98,7 +98,9 @@ def limpiar_datos_por_modulo(modulo: str, df: pd.DataFrame) -> pd.DataFrame:
 
         # Asegurar tipo de fecha
         archivo_metas["FECHA"] = pd.to_datetime(archivo_metas["FECHA"]).dt.date
-        archivo_metas["META EQUIPO A LA FECHA"] = archivo_metas["META EQUIPO A LA FECHA"].astype(int)
+        archivo_metas["META EQUIPO A LA FECHA" = (
+            pd.to_numeric(archivo_metas["META EQUIPO A LA FECHA").fillna(0).astype(int)
+        )
 
         # Filtrar metas por fecha
         metas_dia = archivo_metas[archivo_metas["FECHA"] == fecha_referencia]
