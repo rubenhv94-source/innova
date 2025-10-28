095d27a30d3b17c6406e78ee8e2dba980eb4f9d8
rubenhv94-source <rubenhv.94@gmail.com>
2025-10-28 01:42:50 -0500

    Delete DIAN_VA/tests directory

--- DIAN_VA/tests/test_logic.py
+++
@@ -1,7 +0,0 @@
-from utils.logic import clasifica_categoria
-
-def test_categoria_analista():
-    assert clasifica_categoria(-1, "Analistas") == "Al día"
-    assert clasifica_categoria(5, "Analistas") == "Atraso normal"
-    assert clasifica_categoria(20, "Analistas") == "Atraso medio"
-    assert clasifica_categoria(50, "Analistas") == "Atraso alto"
