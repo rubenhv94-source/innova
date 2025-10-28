32f09c7517e71ebd608e6a36a09f8bd5941b209f
rubenhv94-source <rubenhv.94@gmail.com>
2025-10-28 01:36:05 -0500

    Update enums.py

--- DIAN_VA/utils/enums.py
+++ DIAN_VA/utils/enums.py
@@ -1,1 +1,13 @@
+from enum import Enum
 
+class Modulo(str, Enum):
+    ANALISTAS = "Analistas"
+    SUPERVISORES = "Supervisores"
+    EQUIPOS = "Equipos"
+
+class Estado(str, Enum):
+    ASIGNADA = "asignada"
+    DEVUELTA = "devuelta"
+    CALIFICADA = "calificada"
+    APROBADA = "aprobada"
+    AUDITADA = "auditada"
