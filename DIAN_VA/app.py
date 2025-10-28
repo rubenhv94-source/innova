56f9de7bef456302c6e6071a7ed13a9caa2862a2
rubenhv94-source <rubenhv.94@gmail.com>
2025-10-28 01:39:41 -0500

    Update app.py

--- DIAN_VA/app.py
+++ DIAN_VA/app.py
@@ -1,1 +1,4 @@
-
+from utils.data import cargar_datos
+from utils.logic import business_days_since_start, get_meta_por_sujeto
+from utils.filters import aplicar_filtro_categoria_transversal
+from utils.charts import grafico_avance_total
